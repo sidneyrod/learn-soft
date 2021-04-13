2770768fbec7006f3d4c7048464abf8b80347505
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-04-13 12:55:19 -0300

    Project created

---
+++ backend/src/main/java/com/sid/learnsoft/LearnsoftApplication.java
@@ -0,0 +1,13 @@
+package com.sid.learnsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnsoftApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sid/learnsoft/LearnsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.learnsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
