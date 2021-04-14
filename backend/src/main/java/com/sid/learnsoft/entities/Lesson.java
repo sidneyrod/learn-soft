c11b3ce38774c22202612034f734c26a5d56e757
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-04-14 16:56:15 -0300

    Deliver

--- backend/src/main/java/com/sid/learnsoft/entities/Lesson.java
+++ backend/src/main/java/com/sid/learnsoft/entities/Lesson.java
@@ -1,7 +1,9 @@
 package com.sid.learnsoft.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -14,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -37,12 +40,14 @@ public abstract class Lesson implements Serializable {
 	@JoinTable(
 		name = "tb_lessons_done",
 		joinColumns = @JoinColumn(name = "lesson_id"),
-		inverseJoinColumns = { @JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id")}
+		inverseJoinColumns = { @JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id") }
 		)
 	private Set<Enrollment> enrollmentsDone = new HashSet<>();
 	
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();
+	
 	public Lesson() {
-		
 	}
 
 	public Lesson(Long id, String title, Integer position, Section section) {
@@ -87,6 +92,10 @@ public abstract class Lesson implements Serializable {
 
 	public Set<Enrollment> getEnrollmentsDone() {
 		return enrollmentsDone;
+	}
+	
+	public List<Deliver> getDeliveries() {
+		return deliveries;
 	}
 
 	@Override
--- backend/src/main/java/com/sid/learnsoft/entities/Notification.java
+++ backend/src/main/java/com/sid/learnsoft/entities/Notification.java
@@ -3,6 +3,7 @@ package com.sid.learnsoft.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Notification implements Serializable {
 	private Long id;
 	
 	private String text;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private boolean read;
 	private String route;
